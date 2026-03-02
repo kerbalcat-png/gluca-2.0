8523d1444616fe45db09a872edf6e85c4631ff67
Marley <marley.cox.915@accesscreative.ac.uk>
2026-03-02 14:54:37 +0000

    feat and fix: fixed home page and added recipe jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home.jsx";
 import About from "./About.jsx";
+import Recipes from "./Recipes.jsx"
 // import Scan from "./Scan.jsx";
 import "./App.css";
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/recipes" element={<Recipes />} />
         {/* <Route path="/scan" element={<Scan />} /> */}
       </Routes> 
     </BrowserRouter>
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -12,12 +12,12 @@ function Home() {
 
       <nav className="navbar">
         <div className="logo">
-          <img src={logo} alt="Gluca Logo" className="max-h-full" />
+          <img src={logo} alt="Gluca Logo" className="max-h-full bg-no-repeat" />
         </div>
         <div className="title">GLUCA</div>
         <ul className="nav-links">
           <li><Link to="/about">About</Link></li>
-          <li><Link to="/scan">Scan Barcode</Link></li>
+          <li><Link to="/recipes">Recipes</Link></li>
         </ul>
       </nav>
 
